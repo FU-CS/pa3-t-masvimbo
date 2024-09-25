2bc5986200e03e4ffa9c51be4caf1af1ad83eeaa
Tany <masvta0@furman.edu>
2024-09-24 22:29:59 -0400

    Changes added, tests passes

--- app/src/main/java/pa3/Queue.java
+++ app/src/main/java/pa3/Queue.java
@@ -1,27 +1,49 @@
 
 package pa3;
 
+
 /**
  * A queue of nodes.
  */
 public class Queue {
-
     /** 
      * Constructs an empty queue.
      */
-    public Queue() {
+    public Node [] arr;
+    private int tracker; 
+    private int capacity;
+    
 
+    public Queue () {
+        this.tracker = 0;
+        this.capacity = 10;
+        this.arr = new Node[capacity];
         
     }
-
     /**
      * Adds a node to the queue.
      * @param node
      */
     public void enqueue(Node node) {
 
-        
+        if (this.tracker >= this.capacity){
+            this.arr[tracker] = node;
+            tracker = tracker + 1;
+        }
+        else{
+            this.capacity = this.capacity + 5;
+            Node [] previous; 
+            previous = this.arr;
+            this.arr = new Node[this.capacity];
+            
+            for (int i = 0; i < this.tracker; i++){
+                this.arr[i] = previous[i];
+            }
+        }
+        this.arr[tracker] = node;
+        this.tracker = this.tracker + 1;
     }
+
 
     /**
      * Removes and returns the node at the front of the queue.
@@ -29,7 +51,16 @@ public class Queue {
      */
     public Node dequeue() {
 
+        if (this.tracker == 0) return null;
         
+        Node firstnode = this.arr[0];
+        for(int i = 0; i < this.tracker -1; i ++){
+            this.arr[i] = this.arr[i+1];
+        }
+
+        this.tracker = this.tracker - 1;
+    
+        return firstnode; 
     }
 
     /**
@@ -37,7 +68,7 @@ public class Queue {
      * @return true if the queue is empty.
      */
     public boolean isEmpty() {
-        
+        return this.tracker == 0;   
     }
 
     public static void main(String[] args) {
